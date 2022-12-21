cf3b9151c448323a8c87da8fa206770e75b6137d
somang1206 <dlthakd1206@naver.com>
2022-12-21 14:34:21 +0900

    gallery+footer

---
+++ js/gallery.js
@@ -0,0 +1,37 @@
+
+"use strict";
+
+
+/*swiper*/
+
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+    delay: 0,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+},
+    speed:3000,
+    loop:true,
+    slidesPerView: "auto",
+    loopedSlides: 6,
+    observer:true, 
+    observeParents:true,
+    spaceBetween : 30,
+    grabCursor: true,
+    loopAdditionalSlides : 1,
+});
+
+
+
+/*mouseover*/
+
+const slide = document.querySelector('.swiper-container');
+
+slide.addEventListener('mouseover', () => {
+    swiper.autoplay.stop();
+});
+slide.addEventListener('mouseleave', () => {
+    swiper.autoplay.start();
+});
+
+
